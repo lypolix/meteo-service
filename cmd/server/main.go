a70ffe6083469bd1b8f8ece88b080324fe3372b2
Polina <plusik-006@mail.ru>
2025-10-22 21:38:15 +0300

    added http-server

---
+++ cmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+
+func main()  {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("welcome"))
+		if err != nil {
+			log.Println(err)
+		}
+	})
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		panic(err)
+	}
+}
+
+
+
+
+

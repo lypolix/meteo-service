780dce0c18f11afb0482b8be2564423d1218ad90
Polina <plusik-006@mail.ru>
2025-10-22 21:43:31 +0300

    added consts

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,18 +8,19 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+const httpPort = ":3000"
 
 func main()  {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		_, err := w.Write([]byte("welcome"))
 		if err != nil {
 			log.Println(err)
 		}
 	})
-	err := http.ListenAndServe(":3000", r)
+	err := http.ListenAndServe(httpPort, r)
 	if err != nil {
 		panic(err)
 	}
